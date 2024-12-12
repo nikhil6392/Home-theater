601599f35500a4c74eb325e43f1e67008fa5a956
nikhil6392 <nikhilpathak210431@gmail.com>
2024-12-13 00:05:11 +0530

    Deifining the metadata of movie

---
+++ metadata/pkg/metadata.go
@@ -0,0 +1,9 @@
+package model
+
+//Defining the metaData of the application
+type Metadata struct {
+	ID string `json:"id"`
+	Title string `json:"title`
+	Description string `json:"description`
+	Director string `json:"director"`
+}
