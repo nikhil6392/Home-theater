4de9efa8514fc92d66c82dfa395f2d7d9afe0ad6
nikhil6392 <nikhilpathak210431@gmail.com>
2024-12-23 16:16:32 +0530

    model created for the rating service

---
+++ rating/pkg/rating.go
@@ -0,0 +1,30 @@
+package model
+
+// RecordID defines a record id. Together with
+//RecordType identifies unique records across all types
+type RecordID string 
+
+// RecordID defines a record type. TOgether with RecordID
+// identifies unique records across all types.
+type RecordType string
+
+// Existing record types.
+
+const (
+	RecordTypeMovie = RecordType("movie")
+)
+
+// UserID defines a user id.
+type UserID string
+
+// RatingValue defines a value of a rating record
+type RatingValue int
+
+// Rating defines an individual rating created by a user for some record.
+
+type Rating struct {
+	RecordID string `json:"recordId"`
+	RecordType string `json:"recordType"`
+	UserID     string  `json:"userId"`
+	Value      string   `json:"value"`
+}
